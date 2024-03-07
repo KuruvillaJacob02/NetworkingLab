e8c4b08a648a665d9f841af19b6c0c5238e3c3d3
Kuruvilla Jacob <36655830+KuruvillaJacob02@users.noreply.github.com>
2024-03-07 14:41:17 +0530

    Add files via upload

---
+++ Day 3/client.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE] = {0};
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Enter message for server: ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+    send(client_socket, buffer, strlen(buffer), 0);
+    printf("Message sent to server\n");
+
+    recv(client_socket, buffer, BUFFER_SIZE, 0);
+    printf("Server response: %s\n", buffer);
+    
+    close(client_socket);
+
+    return 0;
+}
+
---
+++ Day 3/server.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_size = sizeof(client_addr);
+    char buffer[BUFFER_SIZE] = {0};
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);	//Creates server Socket
+    if (server_socket == -1) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    server_addr.sin_family = AF_INET;  //Stores Socket address
+    server_addr.sin_addr.s_addr = INADDR_ANY;  //Stores IP address
+    server_addr.sin_port = htons(PORT);  //Stores Port Number
+
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_socket, 5) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Accept a connection from a client
+    client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size);
+    if (client_socket < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive data from client and send response
+    recv(client_socket, buffer, BUFFER_SIZE, 0);
+    printf("Received message from client: %s\n", buffer);
+    send(client_socket, buffer, strlen(buffer), 0);
+    printf("Message sent to client\n");
+
+    close(client_socket);
+    close(server_socket);
+
+    return 0;
+}
+
