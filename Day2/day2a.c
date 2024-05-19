43d5c5a236ea60cf03247cb5fa1dd6517fbf0151
Kuruvilla Jacob <36655830+KuruvillaJacob02@users.noreply.github.com>
2024-05-19 15:47:23 +0530

    Update day2a.c

--- Day2/day2a.c
+++ Day2/day2a.c
@@ -1,34 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h> 
-#include <sys/wait.h> 
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>  //For Fork,exec calls
+#include<sys/types.h>
+#include<sys/wait.h>
 void main(){
-	int pid = fork();
-	if (pid == -1){
-		fprintf(stderr,"Fork Failed\n");	//FORK FAILED
-		exit(1);
-	}
-	
-	else if (pid == 0){	//Child Process is Executing
-		printf("This is the child and its PID = %d\n",getpid());
-		char *args[] = {"ls", NULL}; //	EXEC Command "ls"
-        	execvp(args[0], args);
-		exit(0);
-	}
-	
-	else if (pid > 1){ 	//Parent Process is Executing
-		int status;
-		printf("This is a parent and its PID = %d. The child is %d\n",getpid(),pid);
-		
-		pid_t child_pid;
-		child_pid = wait(&status);	//Waits for child to finish executing
-		
-		if (WIFEXITED(status))		//Check termination status of child process
-			printf("Child process with PID %d exited with status: %d\n", child_pid, WEXITSTATUS(status));
-		else
-			printf("Child process with PID %d terminated abnormally\n", child_pid);
-		
-	}
+    int pid = fork();
+    if (pid == -1){
+        fprintf(stderr,"Fork Failed");
+        exit(1);
+    }
+
+    else if (pid == 0){
+      printf("This is the child with PID = %d\n",getpid()); //Child Executes here
+      char * args[] = {"ls","-l",NULL};
+      execvp(args[0],args);
+      exit(0);
+    }
+    
+    else{
+        int status;
+        printf("This is the parent with PID = %d\n",getpid());  //Parent Process Executing
+        pid_t child_pid = wait(&status);  //Waits for Child to Finish
+        if (WIFEXITED(status)){
+            printf("Child %d Exited successfully with status %d",child_pid,WEXITSTATUS(status));
+        }
+        else
+            printf("Child terminated abnormally");   
+    }
 }
